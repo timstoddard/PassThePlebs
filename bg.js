491746969895eb541c339903b1e2bbc6fd33ea09
trackstar200 <tim.stoddard2@gmail.com>
2017-01-02 22:24:10 -0800

    initial commit

---
+++ bg.js
@@ -0,0 +1,22 @@
+chrome.runtime.onMessage.addListener(
+  function (request, sender, callback) {
+    if (request.action == 'xhttp') {
+      var xhttp = new XMLHttpRequest();
+      var method = request.method ? request.method.toUpperCase() : 'GET';
+
+      xhttp.onload = function() {
+        callback(xhttp.responseText);
+      };
+      xhttp.onerror = function() {
+        // invoke the callback to clean up the communication port
+        callback('error with: ' + request.url);
+      };
+      xhttp.open(method, request.url, true);
+      if (method == 'POST') {
+        xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      }
+      xhttp.send(request.data);
+      return true; // prevents the callback from being called too early on return
+    }
+  }
+);
---
+++ main.js
@@ -0,0 +1,92 @@
+$('.select-course table tbody tr .sectionNumber').each(function () {
+  var nameElem = $(this).next().next().next();
+  getPolyratingData(nameElem);
+});
+
+function getPolyratingData(nameElem) {
+  var names = nameElem[0].innerText.split(',');
+  var lastName = removeAllSingleLetters(names[0].trim());
+  var firstName = onlyFirstName(removeAllSingleLetters(names[1].trim()));
+  var name = firstName + ' ' + lastName;
+  chrome.runtime.sendMessage(
+    {
+      method: 'GET',
+      action: 'xhttp',
+      url: 'http://polyratings.com/search.php?type=ProfName&terms=' + name.replace(/ /g, '+') + '&format=long&sort=name',
+      data: '',
+    },
+    function (response) {
+      if (response != 'error') {
+        var polyratingPage = $($.parseHTML(response));
+        try {
+          var rating = polyratingPage.find('.hidden-xs span .text-primary')[0].innerText;
+          var profId = response.match(/profid=(\d+)/)[1];
+          var href = 'http://polyratings.com/eval.php?profid=' + profId;
+          var numericalRating = parseFloat(rating, 10);
+          nameElem.css('background', calculateBgRGBA(numericalRating));
+          nameElem.html('<a href="' + href + '" target="_blank" class="ratingLink">' + nameElem.html() + '<br><span class="rating">' + rating + '</span></a>');
+
+        } catch (e) {
+          getPolyratingData2ndAttempt(nameElem, lastName);
+        }
+      } else {
+        getPolyratingData2ndAttempt(nameElem, lastName);
+      }
+    }
+  );
+}
+
+function getPolyratingData2ndAttempt(nameElem, lastName) {
+  chrome.runtime.sendMessage(
+    {
+      method: 'GET',
+      action: 'xhttp',
+      url: 'http://polyratings.com/search.php?type=ProfName&terms=' + lastName + '&format=long&sort=name',
+      data: '',
+    },
+    function (response) {
+      if (response != 'error') {
+        var polyratingPage = $($.parseHTML(response));
+        try {
+          var rating = polyratingPage.find('.hidden-xs span .text-primary')[0].innerText;
+          var profId = response.match(/profid=(\d+)/)[1];
+          var href = 'http://polyratings.com/eval.php?profid=' + profId;
+          var numericalRating = parseFloat(rating, 10);
+          nameElem.css('background', calculateBgRGBA(numericalRating));
+          nameElem.html('<a href="' + href + '" target="_blank" class="ratingLink">' + nameElem.html() + '<br><span class="rating">' + rating + '</span></a>');
+        } catch (e) {
+          addLinkToSearchPage(nameElem, lastName);
+        }
+      } else {
+        addLinkToSearchPage(nameElem, lastName);
+      }
+    }
+  );
+}
+
+function onlyFirstName(name) {
+  var spaceIndex = name.indexOf(' ');
+  return spaceIndex > -1 ? name.substr(0, spaceIndex) : name;
+}
+
+function removeAllSingleLetters(name) {
+  var modified = '';
+  var parts = name.split(' ');
+  parts.forEach(function (part) {
+    if (/[a-z]{2,}/.test(part)) {
+      modified += ' ' + part;
+    }
+  });
+  return modified.trim();
+}
+
+function calculateBgRGBA(rating) {
+  var r = rating < 3 ? 255 : 255 * (4 - rating) / 4;
+  var g = rating < 3 ? 255 * rating / 3 : 255;
+  return 'rgba(' + Math.round(r) + ',' + Math.round(g) + ',0,0.6)';
+}
+
+function addLinkToSearchPage(nameElem, lastName) {
+  var href = 'http://polyratings.com/search.php?type=ProfName&terms=' + lastName + '&format=long&sort=name';
+  nameElem.html('<a href="' + href + '" target="_blank" class="ratingLink">' + nameElem.html() + '</a>');
+}
