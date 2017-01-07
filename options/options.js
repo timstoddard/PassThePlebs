38b2558676bc547c487da382900f95acafc88b22
trackstar200 <tim.stoddard2@gmail.com>
2017-01-07 11:59:57 -0800

    make options.js more robust

--- options/options.js
+++ options/options.js
@@ -1,47 +1,56 @@
-(function () {
-  var showBackgroundColorsElem = document.getElementById('showBackgroundColors');
-  var hideClosedClassesElem = document.getElementById('hideClosedClasses');
-  var hideCancelledClassesElem = document.getElementById('hideCancelledClasses');
-  var hideConflictingClassesElem = document.getElementById('hideConflictingClasses');
-  var restoreDefaultsButton = document.getElementById('restoreDefaults');
-  chrome.storage.sync.get([
-    'showBackgroundColors',
-    'hideClosedClasses',
-    'hideCancelledClasses',
-    'hideConflictingClasses'
-  ], function (options) {
-    showBackgroundColorsElem.checked = value(options['showBackgroundColors'], true);
-    hideClosedClassesElem.checked = value(options['hideClosedClasses'], false);
-    hideCancelledClassesElem.checked = value(options['hideCancelledClasses'], true);
-    hideConflictingClassesElem.checked = value(options['hideConflictingClasses'], false);
-  });
+// defaults
+var SHOW_BACKGROUND_COLORS_DEFAULT = true;
+var HIDE_CLOSED_CLASSES_DEFAULT = false;
+var HIDE_CANCELLED_CLASSES_DEFAULT = true;
+var HIDE_CONFLICTING_CLASSES_DEFAULT = false;
 
-  showBackgroundColorsElem.addEventListener('click', function () {
-    chrome.storage.sync.set({ showBackgroundColors: showBackgroundColorsElem.checked });
-  });
-  hideClosedClassesElem.addEventListener('click', function () {
-    chrome.storage.sync.set({ hideClosedClasses: hideClosedClassesElem.checked });
-  });
-  hideCancelledClassesElem.addEventListener('click', function () {
-    chrome.storage.sync.set({ hideCancelledClasses: hideCancelledClassesElem.checked });
-  });
-  hideConflictingClassesElem.addEventListener('click', function () {
-    chrome.storage.sync.set({ hideConflictingClasses: hideConflictingClassesElem.checked });
-  });
-  restoreDefaultsButton.addEventListener('click', function () {
-    chrome.storage.sync.set({
-      showBackgroundColors: true,
-      hideClosedClasses: false,
-      hideCancelledClasses: true,
-      hideConflictingClasses: false
-    });
-    showBackgroundColorsElem.checked = true;
-    hideClosedClassesElem.checked = false;
-    hideCancelledClassesElem.checked = true;
-    hideConflictingClassesElem.checked = false;
-  });
+// form elements
+var showBackgroundColorsElem = document.getElementById('showBackgroundColors');
+var hideClosedClassesElem = document.getElementById('hideClosedClasses');
+var hideCancelledClassesElem = document.getElementById('hideCancelledClasses');
+var hideConflictingClassesElem = document.getElementById('hideConflictingClasses');
+var restoreDefaultsButton = document.getElementById('restoreDefaults');
 
-  function value(value, defaultValue) {
-    return value === undefined || value === null ? defaultValue : value;
-  }
-} ());
+// load stored data
+chrome.storage.sync.get([
+  'showBackgroundColors',
+  'hideClosedClasses',
+  'hideCancelledClasses',
+  'hideConflictingClasses'
+], function (options) {
+  showBackgroundColorsElem.checked = value(options['showBackgroundColors'], SHOW_BACKGROUND_COLORS_DEFAULT);
+  hideClosedClassesElem.checked = value(options['hideClosedClasses'], HIDE_CLOSED_CLASSES_DEFAULT);
+  hideCancelledClassesElem.checked = value(options['hideCancelledClasses'], HIDE_CANCELLED_CLASSES_DEFAULT);
+  hideConflictingClassesElem.checked = value(options['hideConflictingClasses'], HIDE_CONFLICTING_CLASSES_DEFAULT);
+});
+
+// add event listeners
+addOptionListener(showBackgroundColorsElem, 'showBackgroundColors');
+addOptionListener(hideClosedClassesElem, 'hideClosedClasses');
+addOptionListener(hideCancelledClassesElem, 'hideCancelledClasses');
+addOptionListener(hideConflictingClassesElem, 'hideConflictingClasses');
+restoreDefaultsButton.addEventListener('click', function () {
+  showBackgroundColorsElem.checked = SHOW_BACKGROUND_COLORS_DEFAULT;
+  hideClosedClassesElem.checked = HIDE_CLOSED_CLASSES_DEFAULT;
+  hideCancelledClassesElem.checked = HIDE_CANCELLED_CLASSES_DEFAULT;
+  hideConflictingClassesElem.checked = HIDE_CONFLICTING_CLASSES_DEFAULT;
+  chrome.storage.sync.set({
+    showBackgroundColors: showBackgroundColorsElem.checked,
+    hideClosedClasses: hideClosedClassesElem.checked,
+    hideCancelledClasses: hideCancelledClassesElem.checked,
+    hideConflictingClasses: hideConflictingClassesElem.checked
+  });
+});
+
+// helper functions
+function addOptionListener(elem, name) {
+  elem.addEventListener('click', function () {
+    var data = {};
+    data[name] = elem.checked;
+    chrome.storage.sync.set(data);
+  });
+}
+
+function value(value, defaultValue) {
+  return value === undefined || value === null ? defaultValue : value;
+}
