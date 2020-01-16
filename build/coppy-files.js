3a5b8c66efdcf278615d9e8a3a2a24678b26b03c
Max Fisher <maxmfishernj@gmail.com>
2020-01-15 23:06:22 -0800

    fixed build tooling to support windows

---
+++ build/coppy-files.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+fs.copyFileSync('./src/manifest.json','./dist/manifest.json')
+const src = './media/icons'
+const dest = './dist/icons'
+if (!fs.existsSync(dest)){
+    fs.mkdirSync(dest)
+}
+fs.readdirSync(src)
+.filter(file => file.split('.')[1] === 'png')
+.forEach(file => fs.copyFile(`${src}/${file}`,`${dest}/${file}`,()=>{}))
+
