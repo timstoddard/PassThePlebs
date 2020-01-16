02188b5c5f6b063b9a37020248a1ca023c1c59e9
Max Fisher <maxmfishernj@gmail.com>
2020-01-15 23:37:03 -0800

    spelling fix

--- build/copy-files.js
+++ build/copy-files.js
@@ -4,7 +4,7 @@ fs.copyFileSync('./src/manifest.json','./dist/manifest.json')
 const src = './media/icons'
 const dest = './dist/icons'
 if (!fs.existsSync(dest)){
-    fs.mkdirSync(dest)
+    //fs.mkdirSync(dest)
 }
 fs.readdirSync(src)
 .filter(file => file.split('.')[1] === 'png')
